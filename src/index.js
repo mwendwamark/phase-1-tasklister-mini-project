3bd409d0e2f8a780a6f3fdaed9e757cdb2f27546
mark mwendwa <mwendwamark310@gmail.com>
2023-03-24 13:46:20 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+const form = document.querySelector('#create-task-form');
+const input = document.querySelector('#new-task-description');
+const list = document.querySelector('#tasks');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const task = input.value;
+
+  const li = document.createElement('li');
+  li.textContent = task;
+
+  list.appendChild(li);
+
+  let button = document.createElement("button")
+  button.textContent = "x"
+  button.removeEventListener()
+});
